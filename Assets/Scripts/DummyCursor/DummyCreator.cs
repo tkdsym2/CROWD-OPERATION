757366b952f23da1ac615d7622632995bfb7f8a7
tkdsym2 <k222.tkd.fms@gmail.com>
2019-06-17 01:33:58 +0900

    Feat: update dummy cursor generator
    prefabの生成方法を少し変更

--- Assets/Scripts/DummyCursor/DummyCreator.cs
+++ Assets/Scripts/DummyCursor/DummyCreator.cs
@@ -9,25 +9,34 @@ public class DummyCreator : MonoBehaviour
     public GameObject dummyPrefab;
     private int dummyNum;
     private float drx, dry, drangle;
+    private GameObject[] dummies;
 
     // Start is called before the first frame update
     void Start()
     {
         modeManager = GetComponent<ModeManager>();
         dummyNum = modeManager.dummyNum;
-        for (int i = 1; i < 10; i++)
-        {
-            GameObject dummy = Instantiate(dummyPrefab) as GameObject;
-            drx = UnityEngine.Random.Range(-9.0f, 9.0f);
-            dry = UnityEngine.Random.Range(-5.0f, 5.0f);
-            drangle = Random.Range(45, 360);
-            dummy.transform.position = new Vector3(drx, dry, drangle);
-        }
+        dummies = new GameObject[dummyNum];
+        GenerateDummyCursor(dummyNum);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    }
+
+    private void GenerateDummyCursor(int _num)
+    {
+        for (int i = 1; i < _num; i++)
+        {
+
+            dummies[i] = Instantiate(dummyPrefab) as GameObject;
+            drx = UnityEngine.Random.Range(-9.0f, 9.0f);
+            dry = UnityEngine.Random.Range(-5.0f, 5.0f);
+            drangle = Random.Range(45, 360);
+            Vector3 pos = new Vector3(drx, dry, drangle);
+            dummies[i].transform.position = new Vector3(drx, dry, drangle);
+            dummies[i].transform.parent = transform;
+        }
     }
 }
