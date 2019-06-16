55f95c15e144836457b8cf61b392411819679409
tkdsym2 <k222.tkd.fms@gmail.com>
2019-06-17 01:33:03 +0900

    Feat: add Dummy Cursor Position Randomizer
    DummyCursorの位置をrandomにする機能を追加

---
+++ Assets/Scripts/DummyCursor/RandomizeDummyCursorPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeDummyCursorPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("r")) RandomizeCursorPos();
+    }
+
+    private void RandomizeCursorPos()
+    {
+        float drx = UnityEngine.Random.Range(-9.0f, 9.0f);
+        float dry = UnityEngine.Random.Range(-5.0f, 5.0f);
+        float drangle = Random.Range(45, 360);
+        gameObject.transform.position = new Vector3(drx, dry, drangle);
+    }
+}
