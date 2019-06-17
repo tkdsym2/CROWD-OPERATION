24691d61b113537306afb5f622b2b0fd353c87e7
tkdsym2 <k222.tkd.fms@gmail.com>
2019-06-17 18:22:48 +0900

    Feat: add user Cursors debugger
    Application内と，実際のPC状で動いているカーソルの位置のデバッガー

---
+++ Assets/Scripts/Debugger/UserCursor/ApplicationCursor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ApplicationCursor : MonoBehaviour
+{
+    public GameObject appCursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = appCursor.transform.position.x;
+        float y = appCursor.transform.position.y;
+        Vector3 pos = Camera.main.WorldToScreenPoint(appCursor.transform.position);
+        pos.x = pos.x - Screen.width/2 + 230;
+        pos.y = pos.y - Screen.height/2 - 40;
+        this.GetComponent<RectTransform>().localPosition = pos;
+        this.GetComponent<Text>().text = "x: " + pos.x.ToString() +
+                ", y: " + pos.y.ToString();
+    }
+}
---
+++ Assets/Scripts/Debugger/UserCursor/SoftwareCursor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoftwareCursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = Input.mousePosition;
+        float x = position.x;
+        float y = position.y;
+        float ax = Input.GetAxis("Mouse X");
+        float ay = Input.GetAxis("Mouse Y");
+
+        this.GetComponent<Text>().text = "Software Cursor Property" +
+                "\nx position: " + x.ToString() +
+                "\ny position: " + y.ToString() +
+                "\nx axis: " + ax.ToString() +
+                "\ny axis: " + ay.ToString();
+    }
+}
