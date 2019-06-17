a4c16e2baad5d0d738876a1dc918bc9038811ba7
tkdsym2 <k222.tkd.fms@gmail.com>
2019-06-17 18:22:19 +0900

    Feat: add public propaties debugger
    全体の設定がどうなってるかを見るDebuggerを作成

---
+++ Assets/Scripts/Debugger/Public/DelayTimeField.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DelayTimeField : MonoBehaviour
+{
+    private InputField inputField;
+    private ModeManager modeManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        modeManager = GetComponent<ModeManager>();
+        inputField = this.GetComponent<InputField>();
+        inputField.text = modeManager.delayTime.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
