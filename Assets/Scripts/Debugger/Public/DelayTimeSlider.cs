9d8a6c9c4956f2e8a226dc956bad964a4c859642
tkdsym2 <k222.tkd.fms@gmail.com>
2019-06-19 00:01:23 +0900

    [WIP]: add delay time slide
    遅延時間を操作するスライダー追加

---
+++ Assets/Scripts/Debugger/Public/DelayTimeSlider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DelayTimeSlider : MonoBehaviour
+{
+    private Slider delaySlider;
+    private ModeManager modeManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        modeManager = GetComponent<ModeManager>();
+        delaySlider = GetComponent<Slider>();
+        delaySlider.value = modeManager.delayTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // delaySlider.value = modeManager.delayTime;
+    }
+
+    public void ValueChangeCheck()
+    {
+        modeManager.delayTime = delaySlider.value / 1000f;
+    }
+}
