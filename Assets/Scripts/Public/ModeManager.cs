66640b9be2faa7d4d4abd8e2502a733ef6b62468
tkdsym2 <k222.tkd.fms@gmail.com>
2019-06-19 00:04:25 +0900

    Feat: add initial Control-Display Ratio
    C/D比の初期値を設定

--- Assets/Scripts/Public/ModeManager.cs
+++ Assets/Scripts/Public/ModeManager.cs
@@ -7,12 +7,14 @@ public class ModeManager : MonoBehaviour
     public bool delayMode;
     public float delayTime;
     public int dummyNum;
+    public float cdr;
 
     // before Start()
     private void Awake()
     {
         delayMode = false;
-        delayTime = 0.0f;
+        delayTime = 0/1000f;
         dummyNum = 12;
+        cdr = 1.0f;
     }
 }
