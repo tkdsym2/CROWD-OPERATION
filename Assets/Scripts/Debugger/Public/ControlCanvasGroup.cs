1f45db2b2028d6853da9e849cea294b1426821d6
tkdsym2 <k222.tkd.fms@gmail.com>
2019-06-19 00:05:22 +0900

    Feat: add debugger view operator
    デバッガを表示するかしないかを"m" を押すことで切り替え

---
+++ Assets/Scripts/Debugger/Public/ControlCanvasGroup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlCanvasGroup : MonoBehaviour
+{
+    public CanvasGroup canvasGroup;
+    private bool isShow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isShow = false;
+        canvasGroup = gameObject.GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("m")) isShow = !isShow;
+
+        if (isShow)
+        {
+            canvasGroup.alpha = 1;
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+        }
+        else
+        {
+            canvasGroup.alpha = 0;
+            canvasGroup.interactable = false;
+            canvasGroup.blocksRaycasts = false;
+        }
+    }
+}
