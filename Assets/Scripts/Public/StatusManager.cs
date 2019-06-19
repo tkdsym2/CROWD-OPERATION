691e423322da5eceeeb24d36edb7bac5c9acb2c5
tkdsym2 <k222.tkd.fms@gmail.com>
2019-06-19 18:41:45 +0900

    Feat: add new status manager class
    基本ここからpropertyを取る

---
+++ Assets/Scripts/Public/StatusManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/**
+ * this manager has all status of applications
+ */
+
+public class StatusManager : MonoBehaviour
+{
+    public float delayTime;// delay time forall cursor
+    public int dummyNum;// number of dummy cursor
+    public float cdr; // control-display ratio
+    public void Awake() {
+        delayTime = 0.0f;
+        dummyNum = 12;
+        cdr = 1.0f;
+    }
+}
