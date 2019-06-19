e48cd328451a4667af9335229aaae2ce0b20c070
tkdsym2 <k222.tkd.fms@gmail.com>
2019-06-19 20:37:26 +0900

    Feat: add viewer of amount of cursor & delay time
    遅延時間とカーソルの量を反映

---
+++ Assets/Scripts/Debugger/Public/CurrentDelayTime.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CurrentDelayTime : MonoBehaviour
+{
+    private Text currentDelayTime;
+    public GameObject statusManager;
+    private StatusManager sm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentDelayTime = GetComponent<Text>();
+        sm = statusManager.GetComponent<StatusManager>();
+        currentDelayTime.text = sm.delayTime.ToString() + "\n" + sm.dummyNum.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentDelayTime.text = (sm.delayTime * 1000).ToString() + " [ms]\n" 
+            + sm.dummyNum.ToString();
+    }
+}
