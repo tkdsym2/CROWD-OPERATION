2a9b2d319eb62e659e7b1bc38409e8201ade0af9
tkdsym2 <k222.tkd.fms@gmail.com>
2019-06-20 17:47:05 +0900

    Feat: add change dummy cursor visual script
    dropdownで選択したカーソル形状に動的に変化

---
+++ Assets/Scripts/DummyCursor/DummyCursorVisual.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DummyCursorVisual : MonoBehaviour
+{
+    private StatusManager sm;
+    private SpriteRenderer spriteRenderer;
+    public Sprite defaultCursor, discoveredCursor;
+    public Sprite circleCursor, discoveredCircleCursor;
+    public Sprite squareCursor, discoveredSquareCursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = GameObject.Find("StatusManager").GetComponent<StatusManager>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = squareCursor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(sm.selectedVisual)
+            {
+                case 0:
+                    spriteRenderer.sprite = squareCursor;
+                    break;
+                case 1:
+                    spriteRenderer.sprite = circleCursor;
+                    break;
+                case 2:
+                    spriteRenderer.sprite = defaultCursor;
+                    break;
+                default:
+                    break;
+            }
+    }
+}
