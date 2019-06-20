ecf3a867d9ccae497a970bb98d4788221f195d43
tkdsym2 <k222.tkd.fms@gmail.com>
2019-06-20 17:04:26 +0900

    Feat: add Cursor Visual Selector
    Debugger Modeの時，カーソルの見た目を変えられるようにした

---
+++ Assets/Scripts/DebuggerView/Public/CursorVisualSelector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorVisualSelector : MonoBehaviour
+{
+    public GameObject statusManager;
+    private StatusManager sm;
+    private Dropdown visualSelector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = statusManager.GetComponent<StatusManager>();
+        visualSelector = GetComponent<Dropdown>();
+        visualSelector.value = sm.selectedVisual;
+    }
+
+    // Update is called once per frame
+    void Update(){}
+
+    public void OnChangeValue()
+    {
+        switch(visualSelector.value)
+        {
+            case 0:
+                sm.selectedVisual = 0;
+                Debug.Log(sm.selectedVisual.ToString());
+                break;
+            case 1:
+                sm.selectedVisual = 1;
+                Debug.Log(sm.selectedVisual.ToString());
+                break;
+            case 2:
+                sm.selectedVisual = 2;
+                Debug.Log(sm.selectedVisual.ToString());
+                break;
+            default:
+                break;
+        }
+    }
+}
