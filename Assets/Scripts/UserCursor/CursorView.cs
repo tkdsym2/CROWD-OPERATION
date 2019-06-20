247965b13ea963f9db7f7f21592d87ce930cce28
tkdsym2 <k222.tkd.fms@gmail.com>
2019-06-20 17:05:55 +0900

    Feat: udpate user cursor visual
    ユーザのカーソルを選択された形状に変更

--- Assets/Scripts/UserCursor/CursorView.cs
+++ Assets/Scripts/UserCursor/CursorView.cs
@@ -7,16 +7,17 @@ public class CursorView : MonoBehaviour
     public GameObject statusManager;
     private StatusManager sm;
     // define sprite
-    SpriteRenderer MainSpriteRenderer;
-    public Sprite defaultCursor;
-    public Sprite discoveredCursor;
+    private SpriteRenderer MainSpriteRenderer;
+    public Sprite defaultCursor, discoveredCursor;
+    public Sprite circleCursor, discoveredCircleCursor;
+    public Sprite squareCursor, discoveredSquareCursor;
     private bool isDiscover = false;
     // Start is called before the first frame update
     void Start()
     {
         sm = statusManager.GetComponent<StatusManager>();
         MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-        MainSpriteRenderer.sprite = defaultCursor;
+        MainSpriteRenderer.sprite = squareCursor;
     }
 
     // Update is called once per frame
@@ -27,11 +28,37 @@ public class CursorView : MonoBehaviour
 
         if (sm.isDiscover)
         {
-            MainSpriteRenderer.sprite = discoveredCursor;
+            switch(sm.selectedVisual)
+            {
+                case 0:
+                    MainSpriteRenderer.sprite = discoveredSquareCursor;
+                    break;
+                case 1:
+                    MainSpriteRenderer.sprite = discoveredCircleCursor;
+                    break;
+                case 2:
+                    MainSpriteRenderer.sprite = discoveredCursor;
+                    break;
+                default:
+                    break;
+            }
         }
         else
         {
-            MainSpriteRenderer.sprite = defaultCursor;
+            switch(sm.selectedVisual)
+            {
+                case 0:
+                    MainSpriteRenderer.sprite = squareCursor;
+                    break;
+                case 1:
+                    MainSpriteRenderer.sprite = circleCursor;
+                    break;
+                case 2:
+                    MainSpriteRenderer.sprite = defaultCursor;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
