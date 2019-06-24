e965f77daf0f093fde8ad8da2daa52f03393891e
tkdsym2 <k222.tkd.fms@gmail.com>
2019-06-25 02:57:38 +0900

    Feat: add DemoManager Object
    Demoシーンに切り替える際に値を保持するManagerを追加

---
+++ Assets/Scripts/Settings/DemoManager/DemoManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DemoManager : MonoBehaviour
+{
+    public float delayTime;// delay time forall cursor[ms]
+    public int dummyNum;// amount of dummy cursor
+    public float cdr;// control-display ratio
+    public int minAngle;
+    public int maxAngle;
+    public int selectedVisual;
+
+    void Awake()
+    {
+        delayTime = 0.0f;
+        dummyNum = 12;
+        cdr = 1.0f;
+        minAngle = 30;
+        maxAngle = 345;
+        selectedVisual = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);// store for all scene
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown("s"))
+        {
+            SceneManager.LoadScene("Demo");
+        }
+    }
+}
