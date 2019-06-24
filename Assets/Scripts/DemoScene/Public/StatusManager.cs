f327093f692c688f61bdd67cf70f40874ea2133a
tkdsym2 <k222.tkd.fms@gmail.com>
2019-06-25 03:01:00 +0900

    Feat: update defining value based on DemoManager
    DemoManagerから引っ張ってきて初期設定を更新

--- Assets/Scripts/DemoScene/Public/StatusManager.cs
+++ Assets/Scripts/DemoScene/Public/StatusManager.cs
@@ -10,28 +10,23 @@ using UnityEngine.SceneManagement;
 public class StatusManager : MonoBehaviour
 {
     public bool isDiscover;// whetherto be discovered self cursor
-    public float delayTime;// delay time forall cursor[ms]
-    public int dummyNum;// number of dummy cursor
-    public float cdr; // control-display ratio
+    public float delayTime;// from demo manager
+    public int dummyNum;// from demo manager
+    public float cdr;// from demo manager
     public bool isShowingDebugger; // debugger view switch
-    public int minRotation; // minimum rotation
-    public int maxRotation;// maximum rotation
+    public int minAngle; // from demo manager
+    public int maxAngle;// from demo manager
     public float discoveredTime;// until discovered time[ms]
     public bool isStarted;// whether to be started recording
     public bool canRandomize;// whether to randomize cursor
-    public int selectedVisual;// selected cursor visual number
+    public int selectedVisual;// from demo manager
+    private DemoManager dm;
     public void Awake() {
         isDiscover = false;
-        delayTime = 0.0f; // second
-        dummyNum = 12;
-        cdr = 1.0f;
         isShowingDebugger = false;
-        minRotation = 30;
-        maxRotation = 345;
         discoveredTime = 0.0f;
         isStarted = false;
         canRandomize = false;
-        selectedVisual = 0;
     }
 
     void Update(){
