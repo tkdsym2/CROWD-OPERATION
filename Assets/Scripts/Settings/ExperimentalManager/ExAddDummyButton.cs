5099f38911f19941bff3d67a6fdbe23f642964fd
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-07 02:34:13 +0900

    Feat: add amount of dummy button
    ダミーセッションを追加するボタン

---
+++ Assets/Scripts/Settings/ExperimentalManager/ExAddDummyButton.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExAddDummyButton : MonoBehaviour
+{
+    public GameObject exManager;
+    private ExperimentalManager em;
+    private List<int> dummyNumSession;
+    private InputField inputField;
+    private Text sessionView;
+    // Start is called before the first frame update
+    void Start()
+    {
+        em = exManager.GetComponent<ExperimentalManager>();
+        dummyNumSession = em.dummyNumSession;
+        inputField = GameObject.Find("DummyClonesAmplifer").GetComponent<InputField>();
+        sessionView = GameObject.Find("CurrentDummySettings").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        if(inputField.text != "") {
+            int _num = int.Parse(inputField.text);
+            dummyNumSession.Add(_num);
+            inputField.text = "";
+            string _temp = "Session: ";
+            for(int i = 0; i < em.dummyNumSession.Count; i++)
+            {
+                _temp = _temp + em.dummyNumSession[i].ToString() + ", ";
+            }
+            sessionView.text = _temp;
+        }
+    }
+}
