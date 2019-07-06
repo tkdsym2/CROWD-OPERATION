29ef4d38077bd60fbb2eb44695c5aa25bf93be47
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-07 02:33:33 +0900

    Feat: add dummy session viewer
    ダミーカーソルの総数，セッションを表示

---
+++ Assets/Scripts/Settings/ExperimentalManager/DummyNumSessionViewer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DummyNumSessionViewer : MonoBehaviour
+{
+    public GameObject experimentalManager;
+    private ExperimentalManager em;
+    private Text text;
+    private string sessionSummary;
+    // Start is called before the first frame update
+    void Start()
+    {
+        em = experimentalManager.GetComponent<ExperimentalManager>();
+        text = gameObject.GetComponent<Text>();
+        string sessionSummary = "Session: ";
+        for(int i = 0; i < em.dummyNumSession.Count; i++)
+        {
+            sessionSummary = sessionSummary + em.dummyNumSession[i].ToString() + ", ";
+        }
+        text.text = sessionSummary;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
