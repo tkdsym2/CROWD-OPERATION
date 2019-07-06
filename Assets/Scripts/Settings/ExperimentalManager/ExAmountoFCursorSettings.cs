d23b8a3ac3ebfc0e34497d73e740c97f49b4fb38
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-07 02:31:53 +0900

    Feat: add amount of cursor operator
    カーソルの個数を制御するオペレータ

---
+++ Assets/Scripts/Settings/ExperimentalManager/ExAmountoFCursorSettings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExAmountoFCursorSettings : MonoBehaviour
+{
+    public InputField inputField;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputField = inputField.GetComponent<InputField>();
+        text = text.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InputNum() {
+        text.text = inputField.text;
+    }
+}
