9e9310d9e96ccafc632c86b4f8187b25ff5047b9
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-08 04:17:33 +0900

    Feat: add StudyManager
    このSceneにおける共有変数等を全て格納

---
+++ Assets/Scripts/Expetiments/Public/StudyManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StudyManager : MonoBehaviour
+{
+    public string subjectName;
+    public bool isStartStudy;
+    private ExperimentalManager em;
+    public void Awake()
+    {
+        subjectName = "your name";
+        isStartStudy = false;
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        initializeSettings();
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+    {
+        initializeSettings();
+    }
+    
+    private void initializeSettings()
+    {
+        em = GameObject.Find("ExperimentalManager").GetComponent<ExperimentalManager>();
+    }
+}
