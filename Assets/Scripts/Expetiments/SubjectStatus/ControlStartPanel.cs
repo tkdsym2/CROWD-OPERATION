69ea2830f6b68d4247a87889f943677eb97ce8e2
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-08 04:18:25 +0900

    Feat: add Control Panel Operator
    名前を入力したら初期UIを消す

---
+++ Assets/Scripts/Expetiments/SubjectStatus/ControlStartPanel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlStartPanel : MonoBehaviour
+{
+    public GameObject studyManager;
+    private StudyManager sm;
+    private CanvasGroup canvasGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = studyManager.GetComponent<StudyManager>();
+        canvasGroup = gameObject.GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 1;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(sm.isStartStudy)
+        {
+            canvasGroup.alpha = 0;
+            canvasGroup.interactable = false;
+            canvasGroup.blocksRaycasts = false;
+        }
+        else
+        {
+            canvasGroup.alpha = 1;
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+        }
+    }
+}
