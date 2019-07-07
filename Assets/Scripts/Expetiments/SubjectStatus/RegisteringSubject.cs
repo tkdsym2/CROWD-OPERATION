03da4f593f22fa35d3a86009d179511a7bca9635
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-08 04:18:56 +0900

    Feat: add Enter Subject Name Scripts
    名前を入力したらStartPanelを決して実験開始

---
+++ Assets/Scripts/Expetiments/SubjectStatus/RegisteringSubject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RegisteringSubject : MonoBehaviour
+{
+    public GameObject studyManager;
+    private StudyManager sm;
+    private InputField subjectNameField;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = studyManager.GetComponent<StudyManager>();
+        subjectNameField = GameObject.Find("SubjectNameField").GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        if(subjectNameField.text != "") {
+            sm.subjectName = subjectNameField.text;
+            sm.isStartStudy = !sm.isStartStudy;
+        }
+    }
+}
---
+++ Assets/Scripts/Expetiments/SubjectStatus/SubjectNameField.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubjectNameField : MonoBehaviour
+{
+    public InputField subjectNameField;
+    public Text subjectName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        subjectNameField = subjectNameField.GetComponent<InputField>();
+        subjectName = subjectName.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnterName()
+    {
+        subjectName.text = subjectNameField.text;
+    }
+}
