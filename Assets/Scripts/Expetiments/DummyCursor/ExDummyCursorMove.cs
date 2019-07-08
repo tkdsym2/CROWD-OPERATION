e5d2b39f0fdc4f1d476da0da69579c419c90cd39
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-09 01:01:45 +0900

    Feat: add DummyCursorMove func
    ダミーカーソルの動きを制御する関数を追加

---
+++ Assets/Scripts/Expetiments/DummyCursor/ExDummyCursorMove.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoveFunction;
+
+public class ExDummyCursorMove : MonoBehaviour
+{
+    private StudyManager sm;
+    private float rx, ry, ax, ay;
+    public Vector3 clone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = GameObject.Find("StudyManager").GetComponent<StudyManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ax = Input.GetAxis("Mouse X");
+        ay = Input.GetAxis("Mouse Y");
+        Vector3 direction = new Vector3(ax,ay, 0) * 0.5f;
+
+        if(!sm.isDelay){
+            StartCoroutine(DummyCursorFunc.DelayCursor(sm.delayTime, () =>
+            {
+                DummyCursorFunc.MoveDummyCursor(gameObject, direction, sm.cdr);
+                clone = gameObject.transform.position;
+            }));
+        } else {
+            DummyCursorFunc.MoveDummyCursor(gameObject, direction, sm.cdr);
+            clone = gameObject.transform.position;
+        }
+    }
+}
