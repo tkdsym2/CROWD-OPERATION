f4ebd00103b1ec1baf4aa5a2fda3a966e969bf17
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-08 14:34:25 +0900

    Feat: add Interval Timer
    Session Interval Timer とそれの提示を管理するControl Panelを追加

---
+++ Assets/Scripts/Expetiments/Timer/IntervalTimer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntervalTimer : MonoBehaviour
+{
+    public GameObject studyManager;
+    private StudyManager sm;
+    private Text timerView;
+    private float intervalTime;
+    private int seconds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = studyManager.GetComponent<StudyManager>();
+        timerView = gameObject.GetComponent<Text>();
+        intervalTime = sm.sessionIntervalTime;// interval is 3 seconds
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(sm.isStartStudy && sm.finishInterval == false){
+            intervalTime -= Time.deltaTime;
+            seconds = (int)intervalTime;
+            timerView.text = seconds.ToString();
+            if(seconds <= 0){
+                sm.sessionIntervalTime = 4.0f;
+                sm.finishInterval = true;
+                return;
+            }
+        }
+    }
+}
