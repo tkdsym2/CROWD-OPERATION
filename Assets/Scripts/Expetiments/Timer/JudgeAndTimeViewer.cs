0b9b80845f0264a8e27cefcb20dc5b9248dfd8dc
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-09 03:19:36 +0900

    Feat: add discovered time view

---
+++ Assets/Scripts/Expetiments/Timer/JudgeAndTimeViewer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JudgeAndTimeViewer : MonoBehaviour
+{
+    public GameObject studyManager;
+    private StudyManager sm;
+    private Text judgeAndTimerText;
+    private float startTime, discoveredTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = studyManager.GetComponent<StudyManager>();
+        judgeAndTimerText = gameObject.GetComponent<Text>();
+        discoveredTime = 0.0f;
+        judgeAndTimerText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartRecording()
+    {
+        startTime = Time.time;
+    }
+
+    public void FinishRecording()
+    {
+        discoveredTime = Time.time - startTime;
+        sm.discoveredTime = (discoveredTime * 1000);
+        judgeAndTimerText.text = sm.discoveredTime.ToString("F2") + "[ms]";
+        sm.isStartSession = !sm.isStartSession;
+        sm.finishInterval = false;
+    }
+}
