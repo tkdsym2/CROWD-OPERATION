dd7766f6c39b278142cfc055f174aa191fffd096
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-09 00:27:03 +0900

    Feat: add user cursor move function
    各セッション開始時にランダム配置，挙動を制御する関数を生成

--- Assets/Scripts/Expetiments/UserCursor/ExCursorMove.cs
+++ Assets/Scripts/Expetiments/UserCursor/ExCursorMove.cs
@@ -8,7 +8,9 @@ public class ExCursorMove : MonoBehaviour
 {
     public GameObject studyManager;
     private StudyManager sm;
-
+    private float rx, ry, ax, ay;
+    private Vector3 ConvertPosition;
+    private float delayTime, cdr;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +21,26 @@ public class ExCursorMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(sm.isStartSession)
+        {
+            ax = Input.GetAxis("Mouse X");
+            ay = Input.GetAxis("Mouse Y");
+            Vector3 direction = new Vector3(ax, ay, 0) * 0.5f;
+
+            if(!sm.isDelay) {
+                StartCoroutine(UserCursorFunc.DelayCursor(sm.delayTime, () =>
+                    {
+                        UserCursorFunc.MoveCursor(gameObject, direction, sm.cdr);
+                    }));
+            } else {
+                UserCursorFunc.MoveCursor(gameObject, direction, sm.cdr);
+            }
+        }
+    }
+
+    public void RandomizeCursorPos() {
+        rx = UnityEngine.Random.Range(-9.0f, 9.0f);
+        ry = UnityEngine.Random.Range(-5.0f, 5.0f);
+        gameObject.transform.position = new Vector3(rx, ry, 0);
     }
 }
