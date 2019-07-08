421dfcdb419cf50af5c0ab1e2458ae8e648538d5
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-08 16:26:26 +0900

    [WIP]: add experimental user cursor property

---
+++ Assets/Scripts/Expetiments/UserCursor/ExCursorMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using MoveFunction;
+
+public class ExCursorMove : MonoBehaviour
+{
+    public GameObject studyManager;
+    private StudyManager sm;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = studyManager.GetComponent<StudyManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Expetiments/UserCursor/ExCursorView.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExCursorView : MonoBehaviour
+{
+    public GameObject studyManager;
+    private StudyManager sm;
+    private SpriteRenderer MainSpriteRenderer;
+    public Sprite defaultCursor, discoveredCursor;
+    public Sprite circleCursor, discoveredCircleCursor;
+    public Sprite squareCursor, discoveredSquareCursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = studyManager.GetComponent<StudyManager>();
+        MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        ChangeCursorVisual(sm.selectedVisual);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && sm.isStartStudy) sm.isDiscover = !sm.isDiscover;
+
+        ChangeCursorVisual(sm.selectedVisual);
+    }
+
+    private void ChangeCursorVisual(int _num)
+    {
+        if (sm.isDiscover)
+        {
+            switch(_num)
+            {
+                case 0:
+                    MainSpriteRenderer.sprite = discoveredSquareCursor;
+                    break;
+                case 1:
+                    MainSpriteRenderer.sprite = discoveredCircleCursor;
+                    break;
+                case 2:
+                    MainSpriteRenderer.sprite = discoveredCursor;
+                    break;
+                default:
+                    break;
+            }
+        }
+        else
+        {
+            switch(_num)
+            {
+                case 0:
+                    MainSpriteRenderer.sprite = squareCursor;
+                    break;
+                case 1:
+                    MainSpriteRenderer.sprite = circleCursor;
+                    break;
+                case 2:
+                    MainSpriteRenderer.sprite = defaultCursor;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
