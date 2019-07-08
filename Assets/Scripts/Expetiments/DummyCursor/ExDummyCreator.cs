24824d1e7383d514ff852609e4b81bb29a8cf885
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-09 00:52:56 +0900

    Feat: add DummyCursor Creator
    Sessionに基づいて作られるDummyCursorとその見た目を変更するスクリプト，GameObjectを追加

---
+++ Assets/Scripts/Expetiments/DummyCursor/ExDummyCreator.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ExDummyCreator : MonoBehaviour
+{
+    private StudyManager sm;
+    public GameObject dummyPrefab;
+    private float drx, dry, drangle;
+    private GameObject[] dummies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = GameObject.Find("StudyManager").GetComponent<StudyManager>();
+        dummies = new GameObject[1];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GenerateDummyCursor(int _num)
+    {
+        DestroyClones(dummies.Length);
+        Array.Clear(dummies, 0, dummies.Length);
+        dummies = new GameObject[sm.dummyNum];
+        for (int i = 0; i < _num; i++)
+        {
+            dummies[i] = Instantiate(dummyPrefab) as GameObject;
+            drx = UnityEngine.Random.Range(-9.0f, 9.0f);
+            dry = UnityEngine.Random.Range(-5.0f, 5.0f);
+            drangle = sm.minAngle + i * (360-2*sm.minAngle)/(float)(_num - 1);
+            Vector3 pos = new Vector3(drx, dry, drangle);
+            dummies[i].transform.position = new Vector3(drx, dry, drangle);
+            dummies[i].transform.parent = transform;
+        }
+    }
+
+    private void DestroyClones(int _num)
+    {
+        for(int i = 0; i < _num; i++){
+            DestroyImmediate(dummies[i]);
+        }
+    }
+}
---
+++ Assets/Scripts/Expetiments/DummyCursor/ExDummyCursorVisual.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExDummyCursorVisual : MonoBehaviour
+{
+    private StudyManager sm;
+    private SpriteRenderer spriteRenderer;
+    public Sprite defaultCursor;
+    public Sprite circleCursor;
+    public Sprite squareCursor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = GameObject.Find("StudyManager").GetComponent<StudyManager>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        ChangeCursorVisual(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeCursorVisual(sm.selectedVisual);
+    }
+
+    private void ChangeCursorVisual(int _num)
+    {
+        switch(_num)
+        {
+            case 0:
+                spriteRenderer.sprite = squareCursor;
+                break;
+            case 1:
+                spriteRenderer.sprite = circleCursor;
+                break;
+            case 2:
+                spriteRenderer.sprite = defaultCursor;
+                break;
+            default:
+                break;
+            }
+    }
+}
