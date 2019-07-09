904483eaa48bd75a378fc76687b1b06a0724cd04
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-10 03:42:33 +0900

    Feat: add csv generator for per session
    セッションごとにcsvを生成する関数を追加．各セッションの最初に生成

--- Assets/Scripts/Expetiments/SubjectStatus/RegisteringSubject.cs
+++ Assets/Scripts/Expetiments/SubjectStatus/RegisteringSubject.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using System.Text;
+using System;
 
 public class RegisteringSubject : MonoBehaviour
 {
@@ -35,8 +38,45 @@ public class RegisteringSubject : MonoBehaviour
         if(subjectNameField.text != "") {
             sm.subjectName = subjectNameField.text;
             sm.isStartStudy = !sm.isStartStudy;
+            GenerateFile(sm.subjectName);
             csp.HideStartPanel();
             itvc.ShowIntervalTimer();
         }
+    }
+
+    private void GenerateFile(string _filename)
+    {
+        string directoryPath = Path.Combine(Application.dataPath, "../data/" + DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss") + "-" + _filename);
+        sm.rootPath = directoryPath;
+        string filepath = Path.Combine(directoryPath, "result.csv");
+    
+        string perSessionAbsDirectoryPath = Path.Combine(directoryPath, "absolute");
+        string perSessionRelDirectoryPath = Path.Combine(directoryPath, "relative");
+
+        StreamWriter sw = null;
+        try {
+            Directory.CreateDirectory(directoryPath);
+            Directory.CreateDirectory(perSessionAbsDirectoryPath);
+            Directory.CreateDirectory(perSessionRelDirectoryPath);
+            sw = File.CreateText(filepath);
+        } catch(Exception e) {
+            Debug.Log (e.Message);
+        } finally {
+            if(sw != null)
+            {
+                try {
+                    sw.Dispose();
+                } catch(Exception e){
+                    Debug.Log(e.Message);
+                }
+            }
+        }
+        FileInfo fi = new FileInfo(filepath);
+        sw = fi.AppendText();
+        string[] column = {"dymmyNum", "delay", "cdr", "minAngle", "maxAngle", "InitX", "InitY","time", "Judge", "AbsolutePos", "RelativePos"};
+        string _column = string.Join(",", column);
+        sw.WriteLine(_column);
+        sw.Flush();
+        sw.Close();
     }
 }
