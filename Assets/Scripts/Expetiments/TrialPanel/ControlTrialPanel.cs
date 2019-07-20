ee4441d92e52798fd4d3ad65460a8cb5f96507e4
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-20 15:44:35 +0900

    Feat: add trial control panel

---
+++ Assets/Scripts/Expetiments/TrialPanel/ControlTrialPanel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlTrialPanel : MonoBehaviour
+{
+    public GameObject studyManager;
+    private StudyManager sm;
+    private CanvasGroup canvasGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = studyManager.GetComponent<StudyManager>();
+        canvasGroup = gameObject.GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowTrialPanel()
+    {
+        canvasGroup.alpha = 1;
+        canvasGroup.interactable = true;
+        canvasGroup.blocksRaycasts = true;
+    }
+
+    public void HideTrialPanel()
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.interactable = false;
+        canvasGroup.blocksRaycasts = false;
+    }
+}
