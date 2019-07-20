a38af44a85e8e44a85aabd05d6714acb76e8a34a
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-20 15:43:38 +0900

    Feat: add trial timer

---
+++ Assets/Scripts/Expetiments/TrialPanel/TimerView.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerView : MonoBehaviour
+{
+    private StudyManager sm;
+    private Text timerView;
+    private float trialTime;
+    private int seconds;
+    public GameObject userCursor;
+    private ExCursorMove ecm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = GameObject.Find("StudyManager").GetComponent<StudyManager>();
+        timerView = gameObject.GetComponent<Text>();
+        ecm = userCursor.GetComponent<ExCursorMove>();
+        trialTime = sm.timeLimit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(sm.isStartStudy && sm.isStartSession) StartCountdown();
+    }
+
+    public void StartCountdown()
+    {
+        trialTime -= Time.deltaTime;
+        seconds = (int)trialTime;
+        timerView.text = seconds.ToString() + "[s]";
+
+        if(seconds <= 0){
+            // ここはまた別の処理が必要
+            ecm.FinishSession();
+            return;
+        }
+    }
+
+    public void ResetTimer()
+    {
+        trialTime = sm.timeLimit;
+        seconds = (int)trialTime;
+    }
+}
