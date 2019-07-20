a60a501186b663d2a2eedc02b65b1057e0342158
tkdsym2 <k222.tkd.fms@gmail.com>
2019-07-20 15:42:35 +0900

    Feat: add Session Count View

---
+++ Assets/Scripts/Expetiments/TrialPanel/SessionView.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SessionView : MonoBehaviour
+{
+    public GameObject studyManager;
+    private StudyManager sm;
+    private int totalSessionNum;
+
+    private Text sessionView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sm = studyManager.GetComponent<StudyManager>();
+        sessionView = gameObject.GetComponent<Text>();
+        totalSessionNum = sm.studySessions.Count;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(sm.isStartSession) ShowSession();
+    }
+
+    private void ShowSession()
+    {
+        sessionView.text = (sm.studySessions.Count + 1).ToString() + "/" + totalSessionNum.ToString();
+    }
+}
